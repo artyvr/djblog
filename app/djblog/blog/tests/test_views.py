98fd4d60308b438fad6649dc24a80e2cdc01fe00
art <artyvr@yandex.ru>
2023-12-17 21:37:07 +0300

    Add new tests

--- app/djblog/blog/tests/test_views.py
+++ app/djblog/blog/tests/test_views.py
@@ -1,9 +1,12 @@
 """ Test blog views """
 
 from django.test import TestCase
-from django.contrib.auth import get_user_model, authenticate
-from blog.models import Post
+from django.contrib.auth import get_user_model
+from blog.models import Post, Tag
 
+USER_NAME = 'test'
+USER_PASSWORD = '23TestUser23'
+USER_EMAIL = 'test@test.com'
 
 class ViewsTestCase(TestCase):
     """ Views tests """
@@ -39,17 +42,32 @@ class ViewsTestCase(TestCase):
         self.assertEqual(response.status_code, 403)
 
     def test_post_detail_load(self):
+        """ The post detail page load (/blog/post/<str:slug>/) """
         user = get_user_model().objects.create_user(
-            username='test',
-            password='23TestUser23',
-            email='test@test.com')
+            username=USER_NAME,
+            password=USER_PASSWORD,
+            email=USER_EMAIL)
         user.save()
-        
         post = Post(title='Test post title',
                     body='Test post body',
                     user=user)
         post.save()
-
         post = Post.objects.get(title__iexact='Test post title')
         response = self.client.get(f'/blog/post/{post.slug}/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_tag_update_load(self):
+        """ The tag update page load (/blog/tag/<str:slug>/update/) """
+        user = get_user_model().objects.create_user(
+            username=USER_NAME,
+            password=USER_PASSWORD,
+            email=USER_EMAIL)
+        user.save()
+        tag = Tag(title='Tag',
+                  slug='test-tag-slug',
+                  user=user)
+        tag.save()
+        tag = Tag.objects.get(title__iexact='Tag')
+        self.client.login(username=USER_NAME, password=USER_PASSWORD)
+        response = self.client.get(f'/blog/tag/{tag.slug}/update/')
         self.assertEqual(response.status_code, 200)
